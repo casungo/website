1e9992d42c19ea4a00d26393bb59e1ca24508354
casungo <casungo@outlook.it>
2024-03-04 21:42:09 +0100

    implemented i18n & cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,22 @@
 import { defineConfig } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://casungo.top",
   prefetch: true,
   integrations: [sitemap()],
+  output: "hybrid",
+  adapter: cloudflare({
+    imageService: "compile",
+  }),
+  i18n: {
+    defaultLocale: "it",
+    locales: ["en", "it"],
+    routing: {
+      prefixDefaultLocale: true,
+    },
+  },
 });
