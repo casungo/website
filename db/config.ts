b0045ff464d31834f3c9891839fd2494a2960dda
casungo <alessandro@casungo.top>
2024-05-19 16:45:01 +0200

    fix email and improved db read

--- db/config.ts
+++ db/config.ts
@@ -1,13 +1,14 @@
-import { defineDb, defineTable, column } from 'astro:db';
+import { defineDb, defineTable, column, NOW } from 'astro:db'
 
 const ContactForm = defineTable({
   columns: {
     from: column.text(),
+    sentAt: column.date({ default: NOW }),
     name: column.text(),
     email: column.text(),
     message: column.text(),
   }
-});
+})
 
 export default defineDb({
   tables: {
