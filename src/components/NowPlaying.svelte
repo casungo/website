01b6d12d22a4912710e11259ab784b3549b5ffaa
casungo <alessandro@casungo.top>
2024-06-18 21:46:03 +0200

    removed api key + astro:env + refactor lastfm

--- src/components/NowPlaying.svelte
+++ src/components/NowPlaying.svelte
@@ -2,28 +2,37 @@
   import { writable } from "svelte/store";
 
   const nowPlaying = writable(null);
-  const loading = writable(true);
+  const isLoading = writable(true);
+  const isPopupVisible = writable(true);
 
   export let title;
   export let by;
   export let notPlayingSomethingText;
 
-  const fetchDataAndUpdate = async () => {
-    try {
-      const response = await fetch(`/api/lastfmnowlistening.json`);
-      const data = await response.json();
-      nowPlaying.set(data.recenttracks.track[0]);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    } finally {
-      loading.set(false);
+  const fetchNowPlayingData = async () => {
+    if (!$isPopupVisible) {
+      isLoading.set(true);
+      try {
+        const response = await fetch(`/api/lastfmnowlistening.json`);
+        const data = await response.json();
+        nowPlaying.set(data.recenttracks.track[0]);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        isLoading.set(false);
+      }
     }
+  };
+
+  const togglePopupVisibility = () => {
+    isPopupVisible.update((visible) => !visible);
+    fetchNowPlayingData();
   };
 </script>
 
 <div class="position-fixed bottom-0 end-0" style="z-index: 7;">
-  <div class="collapse" id="collapseWidthExample">
-    {#if $loading}
+  <div class="collapse" id="nowPlayingPopup">
+    {#if $isLoading}
       <div class="pm-3 mx-3 my-2 card position-sticky top-100 start-100">
         <div class="card-body">
           <div class="spinner-border" role="status">
@@ -40,7 +49,13 @@
           <div class="col-8">
             <div class="card-body">
               <h6 class="card-title">{title}</h6>
-              <p class="card-text"><b><a class="link-body-emphasis link-offset-2 link-underline-opacity-0 link-underline-opacity-75-hover" href={$nowPlaying.url}>{$nowPlaying.name}</a></b></p>
+              <p class="card-text">
+                <b>
+                  <a class="link-body-emphasis link-offset-2 link-underline-opacity-0 link-underline-opacity-75-hover" href={$nowPlaying.url}>
+                    {$nowPlaying.name}
+                  </a>
+                </b>
+              </p>
               <p class="card-text text-body-secondary">
                 {by}
                 <b>{$nowPlaying.artist["#text"]}</b>
@@ -57,14 +72,15 @@
   </div>
   <p>
     <button
-      on:click={fetchDataAndUpdate}
+      on:click={togglePopupVisibility}
       class="position-sticky top-100 start-100 pm-3 mx-3 btn btn-light"
       type="button"
       data-bs-toggle="collapse"
-      data-bs-target="#collapseWidthExample"
+      data-bs-target="#nowPlayingPopup"
       aria-expanded="false"
-      aria-controls="collapseWidthExample"
-      ><i class="bi bi-music-note"></i>
+      aria-controls="nowPlayingPopup"
+    >
+      <i class="bi bi-music-note"></i>
     </button>
   </p>
 </div>
--- src/pages/api/lastfmnowlistening.json.ts
+++ src/pages/api/lastfmnowlistening.json.ts
@@ -1,30 +1,23 @@
 import type { APIRoute } from 'astro';
+import { getSecret } from 'astro:env/server';
 
 export const GET: APIRoute = async () => {
-  const apiKey = '7b6135410a4adc3b3643acc6e429358f';
+  const LastfmApiKey = getSecret('LASTFM_API_KEY');
   const user = 'casungo';
-  const apiUrl = `http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${user}&api_key=${apiKey}&limit=1&format=json`;
+  const apiUrl = `http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${user}&api_key=${LastfmApiKey}&limit=1&format=json`;
 
   try {
-    const apiResponse = await fetch(apiUrl, {
-      method: 'GET',
-      headers: {},
-    });
-
-    const data = await apiResponse.json();
+    const response = await fetch(apiUrl);
+    const data = await response.json();
 
     return new Response(JSON.stringify(data), {
-      status: apiResponse.status,
-      headers: {
-        'Content-Type': 'application/json',
-      },
+      status: response.status,
+      headers: { 'Content-Type': 'application/json' },
     });
-  } catch (error) {
+  } catch {
     return new Response(JSON.stringify({ error: 'Failed to fetch data' }), {
       status: 500,
-      headers: {
-        'Content-Type': 'application/json',
-      },
+      headers: { 'Content-Type': 'application/json' },
     });
   }
 };
