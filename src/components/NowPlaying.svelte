283401e9f041f55a3fd990dfc2901befda8d92c2
casungo <alessandro@casungo.top>
2024-12-29 03:43:20 +0100

    Update NowPlaying.svelte

--- src/components/NowPlaying.svelte
+++ src/components/NowPlaying.svelte
@@ -41,13 +41,7 @@
 </script>
 
 <div class="fixed bottom-0 right-0 z-10">
-  <div 
-    tabindex="0" 
-    role="button" 
-    class="btn mb-4 mr-4 btn-accent shadow-lg hover:shadow-xl transition-all duration-200" 
-    on:click={handleButtonClick} 
-    on:keydown={handleKeyDown}
-  >
+  <div tabindex="0" role="button" class="btn mb-4 mr-4 btn-accent shadow-lg hover:shadow-xl transition-all duration-200" on:click={handleButtonClick} on:keydown={handleKeyDown}>
     <Icon class="text-2xl" icon="material-symbols:music-cast-rounded" />
   </div>
   {#if $isDropdownOpen}
@@ -63,26 +57,16 @@
               <h2 class="card-title text-lg font-bold mb-2">{t("nowPlaying.title")}</h2>
               <div class="flex items-center gap-4">
                 <figure class="w-24 h-24 shrink-0">
-                  <img 
-                    src={$nowPlaying.NowPlayingAlbumArt} 
-                    alt="Album Art" 
-                    class="rounded-lg object-cover w-full h-full" 
-                  />
+                  <img src={$nowPlaying.NowPlayingAlbumArt} alt="Album Art" class="rounded-lg object-cover w-full h-full" />
                 </figure>
                 <div class="min-w-0 flex-auto space-y-1">
                   <div class="text-lg font-semibold truncate">
-                    <a 
-                      class="hover:text-primary transition-colors duration-200" 
-                      href={$nowPlaying.NowPlayingUrl}
-                    >
+                    <a class="hover:text-primary transition-colors duration-200" href={$nowPlaying.NowPlayingUrl}>
                       {$nowPlaying.NowPlayingName}
                     </a>
                   </div>
                   <div class="text-sm text-neutral-500 truncate">
-                    <a
-                      class="hover:text-primary transition-colors duration-200"
-                      href={`https://www.last.fm/music/${encodeURIComponent($nowPlaying.NowPlayingArtist)}`}
-                    >
+                    <a class="hover:text-primary transition-colors duration-200" href={`https://www.last.fm/music/${encodeURIComponent($nowPlaying.NowPlayingArtist)}`}>
                       {$nowPlaying.NowPlayingArtist}
                     </a>
                   </div>
@@ -92,10 +76,7 @@
                 <div class="mt-4 border-t border-neutral-700 pt-2">
                   <p class="text-sm text-neutral-500">{t("nowPlaying.lastPlayedText")}</p>
                   <div class="text-sm truncate">
-                    <a
-                      class="hover:text-primary transition-colors duration-200"
-                      href={$nowPlaying.LastPlayedUrl}
-                    >{$nowPlaying.LastPlayedName}</a>
+                    <a class="hover:text-primary transition-colors duration-200" href={$nowPlaying.LastPlayedUrl}>{$nowPlaying.LastPlayedName}</a>
                   </div>
                 </div>
               {/if}
